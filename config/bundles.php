748584350ef709ffde8ea88d77449eece400c7a3
Joello61 <tchindajoel61@gmail.com>
2026-09-06 23:54:07 +0200

    test: ajouter dama/doctrine-test-bundle pour l'isolation transactionnelle

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
